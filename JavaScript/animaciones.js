2a3e3284ef5ddf9a85ca2aaeb28a6dab020cdedb
Guille <guille.fer79@hotmail.com>
2023-09-14 20:09:46 -0300

    agregamos libreria js para animaciones

---
+++ JavaScript/animaciones.js
@@ -0,0 +1,28 @@
+window.sr =ScrollReveal();
+
+
+    sr.reveal('.contenedor-header', {
+        duration: 2000,
+        origin:'bottom',
+        distance: '-100px',
+    });
+    sr.reveal('.tittle_h1', {
+        duration: 2500,
+        origin:'right',
+        distance: '-100px',
+    });
+    sr.reveal('.contenido_quienesSomos', {
+        duration: 2100,
+        origin:'right',
+        distance: '-100px',
+        reset: true,
+    });
+    sr.reveal('.parrafo_quienesSomos', {
+        duration: 2700,
+        origin:'right',
+        distance: '-100px',
+        reset: true,
+    });
+
+
+    
