77511b068747044c74ecfc8e86b8db18c044ed70
Guille <guille.fer79@hotmail.com>
2023-09-15 14:50:42 -0300

    corregimos efectos js

--- JavaScript/animaciones.js
+++ JavaScript/animaciones.js
@@ -3,31 +3,50 @@ window.sr =ScrollReveal();
 
     sr.reveal('.contenedor-header', {
         duration: 2500,
-        origin:'bottom',
-        distance: '-100px',
+        origin:'top',
+        distance: '100px',
     });
     sr.reveal('.tittle_h1', {
-        duration: 1500,
+        duration: 2000,
         origin:'right',
-        distance: '-100px',
+        distance: '20px',
     });
     sr.reveal('.tittle_h2', {
-        duration: 2500,
+        duration: 1300,
         origin:'left',
         distance: '100px',
         reset: true,
+        delay: 100,
     });
     sr.reveal('.parrafo_quienesSomos', {
-        duration: 2500,
-        origin:'right',
-        distance: '-100px',
+        duration: 2000,
+        origin:'left',
+        distance: '100px',
         reset: true,
+        delay:50,
     });
     sr.reveal('.tittle_h3', {
-        duration: 1500,
-        origin:'right',
-        distance: '-100px',
+        duration: 1300,
+        origin:'left',
+        distance: '100px',
         reset: true,
+        delay: 100,
     });
+    sr.reveal('.parrafoServicios', {
+        duration: 2100,
+        origin:'left',
+        distance: '100px',
+        reset: true,
+        delay: 100,
+    });
+    sr.reveal('.link2', {
+        duration: 2700,
+        origin:'left',
+        distance: '100px',
+        reset: true,
+        delay: 100,
+    });
+    
+    
 
     
